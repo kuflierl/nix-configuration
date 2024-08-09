843376d7cb6ddaa5478fff2f2c5991db5910bffd
kuflierl <41301536+kuflierl@users.noreply.github.com>
2024-08-09 21:55:37 +0200

    some cleanup

--- nixos-templates/raspberrypi4-headless/configuration.nix
+++ nixos-templates/raspberrypi4-headless/configuration.nix
@@ -1,14 +1,8 @@
 { config, pkgs, lib, ... }:
 
 let
-  user = "tempuser";
-  password = "somepass";
-  nixosHardwareVersion = "7f1836531b126cfcf584e7d7d71bf8758bb58969";
-
   extraLocale = "de_DE.UTF-8";
 in {
-  imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/${nixosHardwareVersion}.tar.gz" }/raspberry-pi/4"];
-
   services.openssh.enable = lib.mkDefault true;
 
   i18n = {
@@ -23,15 +17,6 @@ in {
       LC_PAPER = extraLocale;
       LC_TELEPHONE = extraLocale;
       LC_TIME = extraLocale;
-    };
-  };
-
-  users = {
-    mutableUsers = false;
-    users."${user}" = {
-      isNormalUser = true;
-      password = password;
-      extraGroups = [ "wheel" ];
     };
   };
 
