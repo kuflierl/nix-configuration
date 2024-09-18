c989611fccc8ea50173a8b347995c60f10a973c8
kuflierl <41301536+kuflierl@users.noreply.github.com>
2024-09-18 04:09:56 +0200

    try fixing sops path

--- nixos-machines/kul4/sops.nix
+++ nixos-machines/kul4/sops.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.defaultSopsFile = "${toString inputs.self}/secrets/kul4/default.yaml";
+  sops.defaultSopsFile = "secrets/kul4/default.yaml";
   # secrets go here
   sops.secrets."wifi/main_passwd" = {};
 }
