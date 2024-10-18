71754f78f08d80c51e9a17b34a29ea859caa3d8d
kuflierl <41301536+kuflierl@users.noreply.github.com>
2024-10-18 03:40:06 +0200

    fix impermanence machanism for host ssh keys

--- nixos-machines/kul6/impermanence.nix
+++ nixos-machines/kul6/impermanence.nix
@@ -7,10 +7,13 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      "/etc/ssh"
       "/etc/secureboot"
     ];
     files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/machine-id"
       { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
