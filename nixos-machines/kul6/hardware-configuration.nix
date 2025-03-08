6250cdafca65c85eab0e540679514f5f59bae654
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-03-08 21:19:30 +0100

    enable some hw changes

--- nixos-machines/kul6/hardware-configuration.nix
+++ nixos-machines/kul6/hardware-configuration.nix
@@ -1,4 +1,9 @@
 { config, lib, pkgs, ... }:
 {
   fileSystems."/persist".neededForBoot = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.graphics.enable = true;
+  hardware.opengl.enable = true;
 }
