04ea241327bacd7f443962486bd8a5b5f8cf8987
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-03-31 00:27:44 +0200

    enable high dpi sddm

--- nixos-modules/desktop/kde-plasma-6.nix
+++ nixos-modules/desktop/kde-plasma-6.nix
@@ -1,7 +1,10 @@
 { ... }:
 
 {
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    enableHidpi = true;
+    wayland.enable = true;
+  };
   services.desktopManager.plasma6.enable = true;
 }
