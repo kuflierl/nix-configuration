09db7dbe90321e6d9adbb76e95e4bf4958f3104a
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-09-21 01:54:16 +0200

    feat: add formating, convco, git-hooks

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     lanzaboote = {
       url = "github:kuflierl/lanzaboote";
-      # Optional but recommended to limit the size of your system closure.
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -21,9 +20,47 @@
     impermanence = {
       url = "github:nix-community/impermanence";
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, sops-nix, lanzaboote, disko, nixos-hardware, impermanence }@inputs: {
+  outputs = { self, nixpkgs, sops-nix, lanzaboote, disko, nixos-hardware, impermanence, git-hooks, treefmt-nix }@inputs:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    treefmtEval = forAllSystems (
+      system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix
+    );
+  in
+  {
+    formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
+
+    checks = forAllSystems (system: {
+      pre-commit-check = git-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          convco.enable = true;
+          treefmt = {
+            enable = true;
+            packageOverrides.treefmt = self.formatter.${system};
+          };
+        };
+      };
+    });
+
+    devShells = forAllSystems (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        # packages = with nixpkgs.legacyPackages.${system}; [];
+      };
+    });
+
     nixosConfigurations = {
       kul2 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
---
+++ treefmt.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  # Used to find the project root
+  projectRootFile = "flake.nix";
+  # Enable the terraform formatter
+  programs.nixfmt.enable = true;
+
+  #programs.clang-format.enable = true;
+
+  #programs.ruff-format.enable = true;
+}
