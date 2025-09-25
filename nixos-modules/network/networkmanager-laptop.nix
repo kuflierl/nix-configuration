a0c0976e89009e42982177ece37f45edf7047d71
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-09-25 19:36:49 +0200

    feat: enable systemd-resolved for laptops for caching

--- nixos-modules/network/networkmanager-laptop.nix
+++ nixos-modules/network/networkmanager-laptop.nix
@@ -14,6 +14,8 @@ _:
     # enableStrongSwan = true; # iKEw vpns
   };
 
+  services.resolved.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
