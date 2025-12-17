ea85d8bd218f43ec04daa702be8733ed6b576780
kuflierl <41301536+kuflierl@users.noreply.github.com>
2025-12-17 01:35:23 +0100

    refactor: make home dir config dependent

--- home/kuflierl/home.nix
+++ home/kuflierl/home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "kuflierl";
-  home.homeDirectory = "/home/kuflierl";
+  home.homeDirectory = "/home/${config.home.username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
